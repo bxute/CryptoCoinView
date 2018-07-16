efc0b759184af92abb77546a9665c41fd516fb3c
Ankit Kumar <ankit.kumar071460@gmail.com>
2018-07-16 13:34:12 +0530

    fix code format and add comments

--- app/src/main/java/xute/cryptocoinviewexample/MainActivity.java
+++ app/src/main/java/xute/cryptocoinviewexample/MainActivity.java
@@ -1,25 +1,18 @@
 package xute.cryptocoinviewexample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.style.TtsSpan;
-import android.util.Log;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import android.support.v7.app.AppCompatActivity;
 
 import xute.cryptocoinview.CoinView;
 import xute.cryptocoinview.Coins;
-import xute.cryptocoinview.NetworkUtils;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
-		@Override
-		protected void onCreate(Bundle savedInstanceState) {
-				super.onCreate(savedInstanceState);
-				setContentView(R.layout.activity_main);
-				CoinView sbd = findViewById(R.id.coinViewSBD);
-				sbd.setCoinId(Coins.SBD);
-		}
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    CoinView sbd = findViewById(R.id.coinViewSBD);
+    sbd.setCoinId(Coins.SBD);
+  }
 }
